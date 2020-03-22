df489909d095f866f8c73ed3a0527228cd551f4f
yuusiki54 <yuusiki54@icloud.com>
2020-03-22 15:59:05 +0900

    計算機途中

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
-<?php 
-$yuu = "aoyu";
-echo $yuu;
-echo $yuu."さーん。あああああああ";
-echo '<br>';
-for($number = 1;$number <= 10;$number++){
-    echo $number;
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>電卓</title>
+</head>
+
+<?php
+$first = $_POST['first'];
+$second = $_POST['second'];
+$operator = $_POST['operetor'];
+$result = " ";
+
+if (is_numeric($first) && is_numeric($second)) {
+    switch ($operator) {
+        case "足し算":
+    }
 }
+
+
+
+
+
 ?>
