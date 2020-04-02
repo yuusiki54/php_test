ff112412b8a7a4bc2914b90389ec3be5ed32f9e7
yuusiki54 <yuusiki54@icloud.com>
2020-04-02 14:07:40 +0900

    ビンゴシート作成中

--- test.php
+++ test.php
@@ -1,8 +1,26 @@
-<?php
-$block = array(5,10,15);
 
-$average = $sum/count($block);
-echo $average;
-
-
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ビンゴシート</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id ="container">
+        <table>
+            <tr>
+                <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
